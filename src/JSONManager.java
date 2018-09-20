4ff5a59abfa4be6a9f3cb28c639aa3d54654f711
Eduardo Quiroga <ed.quiroga.2103@gmail.com>
2018-09-19 18:39:56 -0600

    Initial commit

---
+++ src/JSONManager.java
@@ -0,0 +1,49 @@
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import 	java.util.Iterator;
+
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+public class JSONManager {
+
+	JSONManager(){}
+
+	public static void main(String[] args){
+		JSONManager manager = new JSONManager();
+
+		System.out.println(manager.write(1,2,3,4));
+
+	}
+	
+	public String write(int x1, int y1, int x2, int y2){
+		
+		JSONObject obj = new JSONObject();
+
+		obj.put("x1",x1);
+        obj.put("y1",y1);
+        obj.put("x2",x2);
+        obj.put("y2",y2);
+
+
+
+		String out = obj.toString();
+
+		return out;
+	}
+
+	public JSONObject getArg(String arg) throws ParseException{
+		JSONParser parser = new JSONParser();
+		
+		JSONObject obj = (JSONObject) parser.parse(arg);
+
+		return obj;
+		
+	}
+}
